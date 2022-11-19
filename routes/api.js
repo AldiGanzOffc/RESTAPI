a58ec0e6cce222da7545d94d8a533c3776e54323
Aldi lesmana <117590981+AldiGanzOffc@users.noreply.github.com>
2022-11-20 02:59:20 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,10 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "Rey"
+var creator = "AldiGanz"
 var neoxr = "yntkts"
 var zeks = "administrator"
-var zeks2 = "apivinz"
+var zeks2 = "apialdi"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
